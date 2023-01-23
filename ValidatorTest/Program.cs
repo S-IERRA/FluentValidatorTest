8fbdede450f3e5dece61a9b9f10e97072efa0a45
S-IERRA <fluxusbanappeals@gmail.com>
2023-01-23 01:24:40 +0100

    init

---
+++ ValidatorTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+using ValidatorTest;
+
+User customer = new() { Email = "testmail.com", Password = "test123", Username = "S-ERRA" };
+
+var result = customer.Validate(customer);
+
+Console.WriteLine(result.IsValid);
+
+foreach(var error in result.Errors)
+	Console.WriteLine(error.ErrorMessage);
---
+++ ValidatorTest/src/User.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorTest
+{
+	public class User : AbstractValidator<User>
+	{
+		public required string Username { get; set; }
+		public required string Email    { get; set; }
+		public required string Password { get; set; }
+
+		public User()
+		{
+			RuleFor(x => x.Username).Length(6, 27).WithMessage("Username must be between 6-27 chars long.");
+			RuleFor(x => x.Email).EmailAddress().WithMessage("Invalid email address.");
+		}
+	}
+}
